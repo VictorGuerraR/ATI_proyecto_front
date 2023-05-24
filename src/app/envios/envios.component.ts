3e107eea54435884712baef19599ed570827f6aa
VictorGuerraR <victorhgrodriguez@gmail.com>
2023-05-23 22:48:52 -0600

    [ADD] Vista envios

--- src/app/envios/envios.component.ts
+++ src/app/envios/envios.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EnviosService } from '../service/envios/envios.service';
 
 @Component({
   selector: 'envios',
@@ -6,7 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./envios.component.css']
 })
 export class EnviosComponent {
+  crearDialog: boolean = false;
+  editarDialog: boolean = false;
+  productosForm: FormGroup;
   
-  constructor(){}
+  constructor(
+    private fb: FormBuilder,
+    private enviosServ: EnviosService
+  ){
+    this.productosForm = this.fb.group({
+      nombre: ['', Validators.required],
+      direccion: ['', Validators.required],
+      telefono: ['', Validators.required]
+    });
+  }
 
+  editarD(params: any) {
+    this.editarDialog = true;
+  }
+
+  actualizarEnvio(){
+
+  }
+  
+  crearD() {
+    this.crearDialog = true;
+  }
+
+  eliminar(params: any){}
+
+  get departamentos(){return this.enviosServ.departamentos}
+  get envios(){return this.enviosServ.envios}
 }
--- src/app/service/envios/envios.service.ts
+++ src/app/service/envios/envios.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnviosService {
 
-  constructor() { }
+  envios: Array<any> = [];
+  departamentos: Array<any> = [];
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.obtenerDepartamentos()
+    this.obtenerEnvios()
+   }
 
   /////---------------------/////
   /////     CREAR
@@ -30,4 +41,28 @@ export class EnviosService {
   /////---------------------/////
   /////     OBTENER
   /////---------------------/////
+  async obtenerEnvios() {
+    try {
+      this.envios = await this.Obenvios().toPromise()
+      console.log(this.envios)
+    } catch (error) {console.log('fallo Obtener envios') }
+  }
+
+  Obenvios(): Observable<any> {
+    return this.http.get<any>('http://localhost:3000/envios/obtener/');
+  }
+
+  /////---------------------/////
+  /////     DEPARTAMENTOS
+  /////---------------------/////
+  async obtenerDepartamentos() {
+    try {
+      this.departamentos = await this.Obdepartamentos().toPromise()
+      console.log(this.departamentos)
+    } catch (error) {console.log('fallo Obtener departamentos') }
+  }
+
+  Obdepartamentos(): Observable<any> {
+    return this.http.get<any>('http://localhost:3000/envios/catalogo/');
+  }
 }
